d63ba393b9d7f03750581625afe6035bf722e2a3
Bobby Shmurner <glitched.ohdeymus@gmail.com>
2025-04-11 11:09:23 +0100

    Added android support to build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,29 @@
-geode build
+param (
+	[string] $platform = "windows"
+)
+
+geode build -p $platform
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
-geode run
+
+if ($platform -eq "windows") {
+	geode run
+} elseif ($platform -eq "android64") {
+	adb push $PSScriptRoot/build-android64/bobby_shmurner.zoom.geode /storage/emulated/0/Android/media/com.geode.launcher/game/geode/mods/bobby_shmurner.zoom.geode
+	adb shell am force-stop com.geode.launcher
+	adb shell am start -D -n "com.geode.launcher/com.geode.launcher.MainActivity"
+
+	$gdpid = $null
+	while ([string]::IsNullOrEmpty($gdpid)) {
+        Start-Sleep -Milliseconds 100
+        $gdpid = adb shell pidof com.geode.launcher
+    }
+
+	try {
+		adb logcat --pid $gdpid
+	}
+	finally {
+		adb shell am force-stop com.geode.launcher
+	}
+} else {
+	Write-Warning "Cannor auto-run $platform"
+}
